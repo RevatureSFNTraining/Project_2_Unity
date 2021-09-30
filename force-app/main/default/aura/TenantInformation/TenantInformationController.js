8ce8700ef88e944ab04049d85e2a60766ce48c57
Julia Weakley <Julia218@me.com>
2021-09-30 14:57:06 -0600

    tenant information is now updateable

--- force-app/main/default/aura/TenantInformation/TenantInformationController.js
+++ force-app/main/default/aura/TenantInformation/TenantInformationController.js
@@ -1,5 +1,24 @@
 ({
-    myAction : function(component, event, helper) {
-
-    }
+	doInit: function(component, event, helper) 
+    {
+		helper.helperMethod(component, event, helper); 
+	},
+    handleClick: function (component, event, helper)
+    {
+    	console.log("Clicked");
+        helper.buttonHelper(component, event, helper);
+	},
+    
+    handleExit: function (component, event, helper)
+    {
+    	 component.set("v.clicked", false);
+	},
+    onSubmit:function (component, event, helper)
+    {
+    	console.log("Submitted");
+         component.set("v.clicked", false);
+	},
+    
+   
+   
 })
