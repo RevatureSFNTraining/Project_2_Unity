e66b7bacc6a6d3fc9582673ed4fd68ed8d1575a5
Wayne He <70216125+wayneh000@users.noreply.github.com>
2021-09-30 16:06:50 -0400

    Added partial search and moved controller logic.

--- force-app/main/default/aura/maintenanceVendors/maintenanceVendorsController.js
+++ force-app/main/default/aura/maintenanceVendors/maintenanceVendorsController.js
@@ -1,25 +1,14 @@
 ({
     initialize : function(component, event, helper) {
-        helper.getSpecialties(component);
-        helper.getVendorCount(component);
-        helper.getVendorList(component, "", "Branch__r.Name", 1, 5);
+        helper.initialize(component);
     },
 
     filter : function(component, event, helper) {
-        var specialty = component.find("selectedSpecialty").get("v.value");
-        var sortField = component.find("sortField").get("v.value");
-        var entriesPerPage = component.find("entriesPerPage").get("v.value");
-
-        helper.getVendorCount(component, specialty);
-        helper.getVendorList(component, specialty, sortField, 1, entriesPerPage);
+        helper.getVendorCount(component);
+        helper.getVendorList(component);
     },
 
-    selectSortAndPage : function(component, event, helper) {
-        var specialty = component.find("selectedSpecialty").get("v.value");
-        var sortField = component.find("sortField").get("v.value");
-        var page = component.find("page").get("v.value");
-        var entriesPerPage = component.find("entriesPerPage").get("v.value");
-
-        helper.getVendorList(component, specialty, sortField, page, entriesPerPage);
+    swapPage : function(component, event, helper) {
+        helper.getVendorList(component, component.find("page").get("v.value"));
     }
 })
--- force-app/main/default/aura/maintenanceVendors/maintenanceVendorsHelper.js
+++ force-app/main/default/aura/maintenanceVendors/maintenanceVendorsHelper.js
@@ -1,9 +1,11 @@
 ({
-    getSpecialties : function(component) {
+    initialize : function(component) {
         var specialtiesMethod = component.get("c.getSpecialties");
         specialtiesMethod.setCallback(this, function(response) {
             if (response.getState() == "SUCCESS") {
                 component.set("v.specialties", response.getReturnValue());
+                this.getVendorCount(component);
+                this.getVendorList(component);
             }
         });
         $A.enqueueAction(specialtiesMethod);
@@ -12,7 +14,8 @@
     getVendorCount : function(component, specialty) {
         var vendorCountMethod = component.get("c.getVendorCount");
         vendorCountMethod.setParams({
-            specialty: specialty
+            search: component.find("search").get("v.value"),
+            specialty: component.find("selectedSpecialty").get("v.value")
         });
         vendorCountMethod.setCallback(this, function(response) {
             if (response.getState() == "SUCCESS") {
@@ -20,19 +23,30 @@
                 var totalPages = Math.ceil(totalRecords / component.find("entriesPerPage").get("v.value"));
                 
                 var pageList = [];
-                for (var i = 1; i <= totalPages; i++)
+                var i = 1;
+                do {
                     pageList.push(i);
+                    i++;
+                } while (i <= totalPages);
+                    
                 component.set("v.pageList", pageList);
+                component.find("page").set("v.value", 1);
             }
         });
         $A.enqueueAction(vendorCountMethod);
     },
 
-    getVendorList : function(component, specialty, sortField, page, entriesPerPage) {
+    getVendorList : function(component, page) {
+        const entriesPerPage = component.find("entriesPerPage").get("v.value");
+
+        if (page === undefined)
+            page = 1;
+
         var vendorListMethod = component.get("c.getVendorList");
         vendorListMethod.setParams({
-            specialty: specialty,
-            orderByField: sortField,
+            search: component.find("search").get("v.value"),
+            specialty: component.find("selectedSpecialty").get("v.value"),
+            orderByField: component.find("sortField").get("v.value"),
             limitRecords: entriesPerPage,
             offset: (page - 1) * entriesPerPage
         });
