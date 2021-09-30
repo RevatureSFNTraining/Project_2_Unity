cec50afa3dc57fd939c41cf87cc90c0fca842093
Julia Weakley <Julia218@me.com>
2021-09-30 08:16:38 -0600

    updates

--- force-app/main/default/aura/LeaseInformation/LeaseInformationController.js
+++ force-app/main/default/aura/LeaseInformation/LeaseInformationController.js
@@ -1,5 +1,21 @@
 ({
-    myAction : function(component, event, helper) {
-
-    }
+	doInit : function(component, event, helper) {
+		let method = component.get("c.getLeaseAgreementInfo");
+       
+        method.setCallback(this, function(response) {
+        	if(response.getState() === "SUCCESS")
+        	{
+                let res = response.getReturnValue(); 
+                 component.set("v.lease", res);
+            	console.log(res); 
+        	}
+        	else 
+            {
+                console.log("Failed with state: " + response.getState());
+            }
+        });
+         
+    	
+    	$A.enqueueAction(method);
+	}
 })
--- force-app/main/default/aura/PropertyInformation/PropertyInformationController.js
+++ force-app/main/default/aura/PropertyInformation/PropertyInformationController.js
@@ -1,5 +1,22 @@
 ({
-    myAction : function(component, event, helper) {
-
+	doInit : function(component, event, helper) {
+		   let method = component.get("c.getPropInfo");
+       
+        method.setCallback(this, function(response) {
+        	if(response.getState() === "SUCCESS")
+        	{
+                let res = response.getReturnValue(); 
+                 component.set("v.prop", res);
+            	console.log(res); 
+        	}
+        	else 
+            {
+                console.log("Failed with state: " + response.getState());
+            }
+        });
+         
+    	
+    	$A.enqueueAction(method);
     }
+                           
 })
