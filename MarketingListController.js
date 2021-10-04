c4577d79f4f7de13e68e6051d01ea9f81e330cdd
Omer Sade <osadepg@gmail.com>
2021-10-04 15:46:54 -0400

    Made component visible in site builder

--- MarketingListController.js
+++ MarketingListController.js
@@ -1,5 +1,8 @@
 ({
-	next : function(component, event, helper) {
+	displayRec : function(component, event, helper) {
+        helper.display(component);
+    },
+    next : function(component, event, helper) {
 		helper.nextProperty(component);
 	},
     previous : function(component, event, helper) {
