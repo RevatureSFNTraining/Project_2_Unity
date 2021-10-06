50a2411f82b76d22788e97e16b46160c0f8bb339
Alex Bashaw <alexander.bashaw@revature.net>
2021-10-06 11:19:37 -0400

    Fixed iteration in propOwner and image output

--- force-app/main/default/aura/propertyOwner/propertyOwnerController.js
+++ force-app/main/default/aura/propertyOwner/propertyOwnerController.js
@@ -2,5 +2,10 @@
 	init : function(component, event, helper) {
 		helper.init(component);
         helper.getAccounts(component);
+	},
+
+	resetForm : function(component, event, helper) {
+		helper.resetForm(component);
+		helper.getAccounts(component);
 	}
 })
