7df25afa63fc25479b98d8aa4f3b01bf9c3701aa
Wayne He <70216125+wayneh000@users.noreply.github.com>
2021-10-06 14:02:15 -0400

    Fixed bug on prev/next buttons.

--- force-app/main/default/aura/PropertyList/PropertyListHelper.js
+++ force-app/main/default/aura/PropertyList/PropertyListHelper.js
@@ -12,27 +12,26 @@
     nextProperty : function(component, event, helper) {
 		var action = component.get("c.getProperty");
         var size = component.get("v.OffsetSize");
-        action.setParams({numBathrooms : component.get("v.oldnumBath"), numBedrooms : component.get("v.oldnumBed"), maxRent : component.get("v.oldnumRent"), setsize : size});
         size += 1;
+        action.setParams({numBathrooms : component.get("v.oldnumBath"), numBedrooms : component.get("v.oldnumBed"), maxRent : component.get("v.oldnumRent"), setsize : size});
         action.setCallback(this, function(response) {
-            component.set("v.OffsetSize", size);
-            component.set("v.properties", response.getReturnValue());
+            if (response.getReturnValue().length > 0) {
+                component.set("v.OffsetSize", size);
+                component.set("v.properties", response.getReturnValue());
+            }
         });
         $A.enqueueAction(action);
 	},
     previousProperty : function(component, event) {
 		var getList = component.get("c.getProperty");
         var size = component.get("v.OffsetSize");
-        if(oss > 1) {
         size -= 1;
-        } else {
-            size = 1;
-        }
-        getList.setParams({numBathrooms : component.get("v.oldnumBath"), numBedrooms : component.get("v.oldnumBed"), maxRent : component.get("v.oldnumRent"), setsize : size - 1});
-        
+        getList.setParams({numBathrooms : component.get("v.oldnumBath"), numBedrooms : component.get("v.oldnumBed"), maxRent : component.get("v.oldnumRent"), setsize : size});
         getList.setCallback(this, function(response) {
-            component.set("v.OffsetSize", size);
-            component.set("v.properties", response.getReturnValue());
+            if (response.getReturnValue().length > 0) {
+                component.set("v.OffsetSize", size);
+                component.set("v.properties", response.getReturnValue());
+            }
         });
         $A.enqueueAction(getList);
     },
