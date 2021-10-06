79b63f402c9527ada46240f4d5a12ec048e6a8ff
Wayne He <70216125+wayneh000@users.noreply.github.com>
2021-10-06 13:55:12 -0400

    Fixed bug on case creation. Removed uesless fields

--- force-app/main/default/aura/TenantIssues/TenantIssuesHelper.js
+++ force-app/main/default/aura/TenantIssues/TenantIssuesHelper.js
@@ -1,10 +1,14 @@
 ({
     formCase : function(component, event, helper) {
         var action = component.get("c.saveCase");
-        action.setParams({description : component.get("v.description"),subject : component.get("v.subject"), reason : component.get("v.reason"), status : component.get("v.status"), orig : component.get("v.origin")});
+        action.setParams({
+            tenantId : component.get("v.tenantId"),
+            description : component.get("v.description"),
+            subject : component.get("v.subject"),
+            reason : component.get("v.reason"),
+        });
         action.setCallback(this,function(response){
-            component.set("v.status", "");
-            component.set("v.origin", "");
+            component.set("v.tenantId", "");
             component.set("v.reason", "");
             component.set("v.subject", "");
             component.set("v.description", "");
